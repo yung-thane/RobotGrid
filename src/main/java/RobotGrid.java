8f6426973775aa927477f9bd06bfe62e0f936aa6
Ronald <ronald.cristobal.lemus@outlook.com>
2022-04-29 09:26:01 -0400

    Initial commit

---
+++ src/main/java/RobotGrid.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RobotGrid {
+    public static void main(String[] args){
+        int columns;
+        int rows;
+        
+        System.out.println("Please enter the amount of rows the grid has.");
+        Scanner inpScan = new Scanner(System.in);
+        columns = inpScan.nextInt();
+
+        System.out.println("Please enter the amount of columns the grid has.");
+        rows = inpScan.nextInt();
+
+
+
+    }
+
+
+}
